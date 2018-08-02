0b016160473439131a873e3a6a62e883d56a2967
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:30:26 +0900

    음수 양수 짝수 홀수 구하기

---
+++ 음수양수짝수 홀수 구하기.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+int main()
+{
+      
+     
+     int a;     
+    scanf("%d", &a);
+    if(a>0)
+    {
+        printf("%s\n","plus");
+         
+        if(a%2==0)
+        {
+            printf("%s\n","even");
+        }
+        else
+        {
+            printf("%s\n","odd");
+        }
+         }     
+    else
+    {
+        printf("%s\n","minus");
+         
+        if(a%2==0)
+        {
+            printf("%s\n","even");
+        }
+        else
+        {
+            printf("%s\n","odd");
+        }
+         }     
+            
+    
+  
+ 
+ 
+    getchar();
+    getchar();
+ 
+     
+ 
+     
+     return 0;
+     
+     
+     
+     
+     
+     
+ 
+}
