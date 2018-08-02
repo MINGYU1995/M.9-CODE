94c5f22cf52460f14d453fbbd9a150c8a70f0a48
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:27:20 +0900

    바둑알 흰돌 놓기

---
+++ 바둑알 흰돌 놓기.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[20][20]={0};
+	int n,i,j,x,y;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d %d",&x,&y);
+		a[x][y]=1;
+		
+	}
+	for(j=1;j<=19;j++){
+
+		for(i=1;i<=19;i++){
+		
+		printf("%d ",a[j][i]);
+		
+		}
+		printf("\n");
+		
+	}
+	
+
+	
+
+
+			
+		
+			
+	
+
+	
+		
+	
+
+	
+		
+	 
+
+				   
+			
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+
+
+}
