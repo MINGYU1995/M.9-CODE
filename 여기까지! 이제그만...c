2562b606aa62e70fea925387250215b4965d8cd8
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:29:56 +0900

    여기까지! 이제그만~..

---
+++ 여기까지! 이제그만...c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a,b,c=0;
+	scanf("%d",&a);
+	for(b=1;a>=b;b++)
+	{
+		c+=b;
+		
+		if(c>=a)break;
+				
+			
+	}
+	printf("%d",c);
+
+
+
+	
+
+	getchar();
+	getchar();
+	getchar();
+	
+
+	return 0;
+
+
+}
+
