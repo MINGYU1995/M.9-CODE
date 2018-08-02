53a661564cdfd5c0f70ddae99afcfab222fa12f6
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:24:09 +0900

    3의 배수는 통과

---
+++ 3의배수는 통과.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int main()
+{
+	int i,a;
+	scanf("%d",&a);
+	for(i=1;a>=i;i++)
+	{
+		if(i%3==0)
+			continue;
+		
+		printf("%d",i);
+	}
+	
+
+
+
+
+	
+
+	getchar();
+	getchar();
+	getchar();
+	
+
+	return 0;
+
+
+}
+
