902a2b598a6ca1883ca020f943f0d3f1848953c8
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:28:17 +0900

    배열 출석횟수 거꾸로 출력하기

---
+++ 배열 출석횟수 거꾸로 출력하기.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[24]={};
+	int i,n,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=n;i>=1;i--)
+	{
+
+	printf("%d ",a[i]);
+
+	}
+		
+	 
+
+				   
+			
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+
+
+}
