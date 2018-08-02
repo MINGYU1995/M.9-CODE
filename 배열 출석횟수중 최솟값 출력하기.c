ee31d9c3059ecc00fe7dea32711d825ca1ca5709
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:29:13 +0900

    배열 출석횟수중 최솟값 출력하기

---
+++ 배열 출석횟수중 최솟값 출력하기.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[24]={};
+	int i,n,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	
+	if(a[i]<n)
+		n=a[i];
+	}
+		
+	printf("%d ",n);
+
+	
+		
+	 
+
+				   
+			
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+
+
+}
