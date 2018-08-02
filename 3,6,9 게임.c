18fb288d753eabfcc52aede09f1632ea564ebc4b
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:21:57 +0900

    Add files via upload

---
+++ 3,6,9 게임.c
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&b);
+	for(a=1;a<=b;a++)
+	{
+		if(a%3==0){
+			printf("X ");
+		}
+		else 
+			printf("%d ",a);
+		
+	}
+
+		
+		
+        
+		
+	
+
+
+
+	
+
+		
+
+	
+	 
+	
+		
+
+		
+	getchar();
+	getchar();
+	getchar();
+	
+
+	return 0;
+
+
+}
