b160290eec5c26a40f6093f8cc8eb1edd3cba790
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:26:26 +0900

    등차수열,등비수열 1 -2 1 8

---
+++ 등차수열,등비수열 1 -2 1 8.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+int main()
+{
+   int a,b,n,m,i;
+   scanf("%d %d %d %d",&a,&m,&b,&n);
+   for(i=0;i<n-1;i++)
+		 {
+			 a*=m;
+			 a+=b;
+
+				   }
+			printf("%d",a);
+
+	getchar();
+	getchar();
+	getchar();
+}
