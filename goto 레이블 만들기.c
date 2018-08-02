452ba6303773ed09a1405ebb4df2e4ca1b6463a4
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:25:34 +0900

    goto 레이블 만들기

---
+++ goto 레이블 만들기.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+int main()
+{
+	 int a,n;
+	 scanf("%d",&a);
+	 reget:
+	 scanf("%d",&n);
+	 printf("%d\n",n);
+	if(--a!=0) goto reget;
+
+
+ 
+
+
+ 
+
+
+	getchar();
+	getchar();
+	getchar();
+
+	
+
+	
+	 return 0;
+	
+	
+	
+	
+	
+
+}
