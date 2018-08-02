dd62854a773bef8694028e7bce4ad86ea4f9e525
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:28:46 +0900

    (배열)출석횟수 구하기

---
+++ 배열 출석횟수 구하기.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[24]={};
+	int i,n,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&j);
+		a[j]=a[j]+1;
+	}
+
+	
+	for(i=1;i<=23;i++)
+		{
+
+		printf("%d ",a[i]);
+
+
+	}
+
+	 
+
+				   
+			
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+
+
+}
