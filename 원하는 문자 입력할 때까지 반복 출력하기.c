d893c8b26746e60c83d48b83e164eb55a0b10826
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-02 23:50:36 +0900

    codeup 기초 100제

---
+++ 원하는 문자 입력할 때까지 반복 출력하기.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+int main()
+{
+	
+	char a;
+	while(a!='q')
+	{
+		scanf("%c ",&a);
+		printf("%c\n",a);
+	}
+
+	 
+	
+		
+
+		
+	getchar();
+	getchar();
+	getchar();
+	
+
+	return 0;
+
+
+}
