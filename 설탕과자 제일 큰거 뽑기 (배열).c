1d3e2ceb89b5fa35cea9cda8b1179599579c3149
MINGYU1995 <42037161+MINGYU1995@users.noreply.github.com>
2018-08-03 21:12:40 +0900

    codeup 기초100제

---
+++ 설탕과자 제일 큰거 뽑기 (배열).c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a[101][101]={};
+	int h,w,k,j,i;
+	int n;
+	int l;
+	int d;
+	int x,y;
+	scanf("%d %d",&h,&w);
+
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+			scanf("%d %d %d %d",&l,&d,&x,&y);
+	
+	
+	for(j=0;j<l;j++)
+	{
+		if(d==0) a[x][y+j]=1;
+		else a[x+j][y]=1;
+		
+	}
+	for(j=0;j<l;j++)
+	{
+		if(d==0) a[x][y+j]=1;
+		else a[x+j][y]=1;
+	}
+	
+	}
+	for(j=1;j<=h;j++)
+	{
+		for(k=1;k<=w;k++){
+			printf("%d ",a[j][k]);
+		}
+		printf("\n");
+	}
+	
+
+	getchar();
+	getchar();
+	getchar();
+	return 0;
+
+
+}
